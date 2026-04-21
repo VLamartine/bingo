1b8349bae7c9ee3e507c3f3fe50f4e379999b99c
Vitor Lamartine <vitorlamartine21@gmail.com>
2026-04-21 18:26:11 -0300

    Premios'

--- prizes.js
+++ prizes.js
@@ -1,13 +1,15 @@
 
 const prizes = [
-    "Premio 01",
-    "Premio 02",
-    "Premio 03",
-    "Premio 04",
-    "Premio 05",
-    "Premio 06",
-    "Premio 07",
-    "Premio 08",
+    "Peça cilíndrica de porcelana diplomática, estampada com dois rivais históricos dividindo o mesmo território como se fosse reunião de condomínio",
+    "Tecido tecnológico de alta absorção, especializado em apagar crimes culinários e resgatar bancadas do caos",
+    "Kit instrumental de precisão dermatológica, projetado para confrontar pontos negros com a seriedade de uma operação cirúrgica",
+    "Recipiente translúcido de boas intenções matinais, com alça azul otimista e uma mensagem que promete energia pela manhã",
+    "Artefato lúdico de tensão psicológica disfarçado de brinquedo, onde cada dentinho é uma aposta perigosa e, a qualquer momento, a criatura decide que sua mão virou jantar",
+    "Cilindro perfumado de calmaria ilusória, aceso na esperança de paz interior enquanto a vida lá fora continua pegando fogo",
+    "Dispositivo portátil de concentração duvidosa, onde um herói tenta manter a dignidade enquanto você luta para acertar argolinhas",
+    "Esfera luminosa de pretensão cósmica, aprisionando a Via Láctea num objeto de cabeceira",
+    "Artefato laranja de comunicação totalmente inútil, equipado com hélice dramática e um painel digno de caça-níquel",
+    // "Premio \"09\""
 ];
 
 export default prizes;
